18b61e5cda7cb8be6852b6bdf1c487b30fcc141f
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-23 19:42:20 +0100

    create basic quote file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { Environment } from 'src/app/shared/environment';
 import { environment } from 'src/environments/environment';
+import { QuoteModule } from 'src/app/quote/quote.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule, QuoteModule, SharedModule],
   providers: [{ provide: Environment, useValue: environment }],
   bootstrap: [AppComponent]
 })
