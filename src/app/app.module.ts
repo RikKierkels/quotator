d4cb97b4bd83c3db5be99ba7a70865551a2473ef
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-23 19:39:07 +0100

    provide environment specific configurations through DI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
+import { Environment } from 'src/app/shared/environment';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [{ provide: Environment, useValue: environment }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
