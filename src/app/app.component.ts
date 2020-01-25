b222827fdf941e36f580b9fa4d05ef9ffd86144c
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-25 10:05:34 +0100

    store a list of favorite quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { QuoteService } from 'src/app/quote/quote.service';
+import { Quote } from 'src/app/quote/quote.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,19 @@ import { QuoteService } from 'src/app/quote/quote.service';
 })
 export class AppComponent implements OnInit {
   quote$ = this.quoteService.quote$;
+  favorites$ = this.quoteService.favorites$;
 
   constructor(private readonly quoteService: QuoteService) {}
 
   ngOnInit(): void {
     this.quoteService.fetchQuoteSubject.next();
+  }
+
+  fetchNewQuote(): void {
+    this.quoteService.fetchQuoteSubject.next();
+  }
+
+  addToFavorites(quote: Quote) {
+    this.quoteService.addToFavorites(quote);
   }
 }
--- src/app/quote/quote.service.ts
+++ src/app/quote/quote.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Environment } from 'src/app/shared/environment';
-import { ReplaySubject } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { Quote } from 'src/app/quote/quote.interface';
 import { STORAGE_KEYS, StorageKeys } from 'src/app/shared/storage.tokens';
@@ -11,8 +11,9 @@ import { StorageService } from 'src/app/shared/storage.service';
   providedIn: 'root'
 })
 export class QuoteService {
-  fetchQuoteSubject = new ReplaySubject<void>(1);
+  private favoritesSubject = new BehaviorSubject<Quote[]>([]);
 
+  fetchQuoteSubject = new ReplaySubject<void>(1);
   quote$ = this.fetchQuoteSubject.pipe(
     switchMap(() => {
       return this.httpClient.get<Quote>(
@@ -21,10 +22,30 @@ export class QuoteService {
     })
   );
 
+  private get favorites(): Quote[] {
+    return this.favoritesSubject.getValue();
+  }
+
+  get favorites$(): Observable<Quote[]> {
+    return this.favoritesSubject.asObservable();
+  }
+
   constructor(
     private readonly httpClient: HttpClient,
     private readonly environment: Environment,
     private readonly storageService: StorageService,
     @Inject(STORAGE_KEYS) private readonly storageKeys: StorageKeys
   ) {}
+
+  addToFavorites(newQuote: Quote) {
+    let favorites = this.favorites;
+
+    if (favorites.some(quote => quote.id === newQuote.id)) {
+      return favorites;
+    }
+
+    favorites = [...favorites, newQuote];
+    this.storageService.set<Quote[]>(this.storageKeys.favorites, favorites);
+    this.favoritesSubject.next(favorites);
+  }
 }
