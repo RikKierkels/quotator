8f2358cc3bd0ec80e0a8f48a117d70eed57fc8c0
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-29 18:05:39 +0100

    add tests for the storage service

--- src/app/shared/storage.service.spec.ts
+++ src/app/shared/storage.service.spec.ts
@@ -1,12 +1,54 @@
-import { TestBed } from '@angular/core/testing';
+import { StorageService } from 'src/app/shared/storage.service';
 
-import { StorageService } from './storage.service';
+const storageMock: jest.Mocked<Storage> = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+  key: jest.fn(),
+  length: 0
+};
 
 describe('StorageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: StorageService;
 
-  it('should be created', () => {
-    const service: StorageService = TestBed.get(StorageService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    service = new StorageService(storageMock);
+  });
+
+  describe('While retrieving an existing item', () => {
+    it('should get the stored item by key', () => {
+      service.get('app.test');
+      expect(storageMock.getItem).toHaveBeenCalledWith('app.test');
+    });
+
+    it('should return the stored item as JSON', () => {
+      const itemToStore = { test: 123 };
+      storageMock.getItem.mockReturnValueOnce(JSON.stringify(itemToStore));
+
+      const storedItem = service.get('app.test');
+
+      expect(storedItem).toEqual(itemToStore);
+    });
+  });
+
+  describe('While retrieving an non-existent item', () => {
+    it('should return a null value', () => {
+      storageMock.getItem.mockReturnValueOnce(undefined);
+      expect(service.get('app.test')).toBe(null);
+    });
+  });
+
+  describe('While storing an item', () => {
+    it('should store the item', () => {
+      const itemToStore = { test: 123 };
+
+      service.set('app.test', itemToStore);
+
+      expect(storageMock.setItem).toHaveBeenCalledWith(
+        'app.test',
+        JSON.stringify(itemToStore)
+      );
+    });
   });
 });
