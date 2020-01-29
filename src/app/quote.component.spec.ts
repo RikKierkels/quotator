aab488f466c9321cb9757edb9df54658767bcd38
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-29 21:25:42 +0100

    add test cases for the quote component

--- src/app/quote.component.spec.ts
+++ src/app/quote.component.spec.ts
@@ -1,16 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
 import { QuoteComponent } from 'src/app/quote.component';
+import { QuoteService } from 'src/app/quote/quote.service';
+import { QuoteSaveService } from 'src/app/quote/quote-save.service';
+import { makeQuote } from 'src/app/quote/quote.test-helpers';
+import { of, Subject } from 'rxjs';
+import { Quote } from 'src/app/quote/quote.interface';
+
+jest.mock('src/app/quote/quote.service');
+jest.mock('src/app/quote/quote-save.service');
 
 describe('QuoteComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [QuoteComponent]
-    }).compileComponents();
-  }));
+  let quoteService: jest.Mocked<QuoteService>;
+  let quoteSaveService: jest.Mocked<QuoteSaveService>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(QuoteComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    // prettier-ignore
+    quoteSaveService = new QuoteSaveService(null, null) as jest.Mocked<QuoteSaveService>;
+    quoteService = new QuoteService(null, null) as jest.Mocked<QuoteService>;
+
+    // @ts-ignore
+    quoteService.fetchQuote = {
+      next: jest.fn()
+    };
+  });
+
+  it('should fetch a quote when the component loads', () => {
+    const component = new QuoteComponent(quoteService, quoteSaveService);
+
+    component.ngOnInit();
+
+    expect(quoteService.fetchQuote.next).toHaveBeenCalledTimes(1);
   });
 });
